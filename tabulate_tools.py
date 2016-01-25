ddd57733432c97145fb9b84880fef53ec70bf990
Roy Hyunjin Han <rhh@crosscompute.com>
2016-01-25 11:09:45 -0500

    Enforce date order

--- tabulate_tools.py
+++ tabulate_tools.py
@@ -62,10 +62,10 @@ def get_date_ranges(from_date, to_date, interval_in_years):
     """
     Retrieve pairs of date ranges based on interval number
     """
-    if not interval_in_years:
-        return [(from_date, to_date)]
     if from_date and to_date and from_date > to_date:
         raise ToolError('to_date must be after from_date')
+    if not interval_in_years:
+        return [(from_date, to_date)]
     date_ranges = []
     date_b = from_date - datetime.timedelta(days=1)
     while date_b < to_date:
