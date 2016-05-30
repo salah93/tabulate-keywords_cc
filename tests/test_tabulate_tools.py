406fc7fc3fbd5f097de3c45f49c2e5ce360bb09d
Salah Ahmed <salah.ahmed.hslc@gmail.com>
2016-05-30 17:23:19 -0400

    test tool

--- tests/test_tabulate_tools.py
+++ tests/test_tabulate_tools.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 from os.path import abspath, dirname, join
 
@@ -6,6 +7,7 @@ TEST_FOLDER = dirname(abspath(__file__))
 # insert tabulate tools in sys path so you can import
 sys.path.insert(0, dirname(TEST_FOLDER))
 from tabulate_tools import get_search_count
+from run import tabulate
 from load_lines import load_unique_lines
 
 
@@ -14,6 +16,18 @@ def test_retstart():
     count, articles_list_20 = get_search_count(expression, retmax=20)
     count, articles_list_100 = get_search_count(expression, retmax=1000)
     assert len(articles_list_20) == len(articles_list_100)
+
+
+def test_run():
+    authors = ['Reshma Jagsi', 'Curtiland Deville', 'Emma Holliday'] 
+    keywords = ['income', 'poverty']
+    mesh = ['social class', 'socioeconomic factors']
+    dates = [(datetime.datetime(1990, 1, 1),
+              datetime.datetime(2015, 12, 31))]
+    res = tabulate(authors, dates, keywords, mesh, True)
+    print(res['search_counts'])
+    print(res['author_articles'])
+    assert res['author_articles']['Reshma Jagsi']
 
 
 def test_load_unique_lines():
